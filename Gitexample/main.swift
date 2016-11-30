28edf0f187284d8fb3c022dc5338c8166d73627a
MacBookPro <MacBookPro@macbookpros-macbook-po.local>
2016-11-30 22:16:56 +0300

    Initial Commit

---
+++ Gitexample/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Gitexample
+//
+//  Created by MacBookPro on 30/11/2016.
+//  Copyright © 2016 MacBookPro. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
